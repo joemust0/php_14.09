b3647d9c7a23810316871681e94fe2398b97b22b
joemust0 <joaofss.1991@gmail.com>
2023-10-10 20:34:53 -0300

    Criação de arquivo inicial

---
+++ src/main.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once __DIR__ . "/../vendor/autoload.php";
+
+    use App\model\Usuario;
+
+    $u = new Usuario("John Wick");
+
+    echo '<h2>Task Today</h2>';
+    echo $u->getNome();
---
+++ src/model/Usuario.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\model;
+
+    class Usuario{
+
+    private $nome;
+
+    function __construct($nome){
+        $this->nome = $nome;
+    }
+
+    public function getNome(){
+        return $this->nome;
+    }
+
+    }
+
+
